3df9f2c340633bd33fed0ebaf356c07889f7b95a
martintognetti <martintognetti@outlook.fr>
2020-11-23 12:23:28 +0100

    exercice.component et service

--- src/app/components/exercice/exercice.component.ts
+++ src/app/components/exercice/exercice.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Exercice } from 'src/app/models/journal/exercice';
+import { ExerciceServiceService } from 'src/app/services/journal/exercice-service.service';
 
 @Component({
   selector: 'app-exercice',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExerciceComponent implements OnInit {
 
-  constructor() { }
+  exercice:Exercice = new Exercice();
+  exercices:Exercice[] = new Array();
+  
+  constructor(private eService: ExerciceServiceService) { }
 
   ngOnInit(): void {
+    this.eService.findAll().subscribe(
+      data => {this.exercices=data}
+    )
+  }
+
+  save():void {
+    this.eService.save(this.exercice).subscribe(arg=> {
+      console.log(arg,"save Ok", arg.status);
+      this.exercice=new Exercice();
+      this.findAll();
+    });
+  }
+ findAll():void {
+    this.eService.findAll().subscribe((data: Exercice[])=>{
+      this.exercices=data;
+      console.log(this.exercice);
+    })
+  }
+  delete(id):void {
+    this.eService.delete(id).subscribe(data=>{
+      this.findAll();
+    })
+  }
+  selectOne(item):void {
+    this.exercice=item;
+    console.log(this.exercice)
+  }
+  deleted(id):void{
+    this.eService.deleted(id,this.exercice).subscribe(data=>{
+      this.findAll();
+    })
+
   }
 
 }
--- src/app/services/journal/exercice-service.service.ts
+++ src/app/services/journal/exercice-service.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Exercice } from 'src/app/models/journal/exercice';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExerciceServiceService {
 
-  constructor() { }
+  private baseURL = "http://localhost:9090/exercices";
+  constructor(private http:HttpClient) { }
+
+  public findAll():Observable<any>{
+    return this.http.get(this.baseURL);
+  }
+
+  public findOne(id:number):Observable<any>{
+    return this.http.get(this.baseURL+"/"+id);
+  }
+
+  public save(exercice:Exercice):Observable<any>{
+    return this.http.post(this.baseURL, exercice);
+  }
+
+  public delete(id:number):Observable<any>{
+    return this.http.delete(this.baseURL+"/"+id)
+  }
+
+  public deleted(id:number, exercice:Exercice):Observable<any>{
+    return this.http.put(this.baseURL+"/"+id, exercice);
+  }
 }

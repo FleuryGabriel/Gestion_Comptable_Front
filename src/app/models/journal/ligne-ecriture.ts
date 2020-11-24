d6f208d7e6c0fe931a818dbd43dd90dbbab3964e
Rainui987 <tetihia@gmail.com>
2020-11-24 12:02:15 +0100

    ligne ecriture update exercice+journal

--- src/app/models/journal/ligne-ecriture.ts
+++ src/app/models/journal/ligne-ecriture.ts
@@ -1,7 +1,15 @@
+import { Exercice } from './exercice';
+import { Journal } from './journal';
+
 export class LigneEcriture {
     libelle:string;
     debit:number;
     credit:number;
     dateEcriture:Date;
     deleted:boolean;
+    exercice:Exercice;
+    exercices:Exercice[];
+    journal:Journal;
+    journaux:Journal[];
+
 }

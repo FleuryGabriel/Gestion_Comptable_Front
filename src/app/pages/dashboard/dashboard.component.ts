2320ddd5954c6c82556448de799655a503670362
Rainui987 <tetihia@gmail.com>
2020-11-26 10:43:35 +0100

    graph update

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -23,22 +23,45 @@ export class DashboardComponent implements OnInit {
   ligneEcritures:LigneEcriture[] = new Array();
   debits=new Array();
   months=new Array();
-
-  public debits12=new Array();
+  public jan:number;
+  public fev:number;
+  public mars:number;
+  public avr:number;
+  public mai:number;
+  public juin:number;
+  public juil:number;
+  public aout:number;
+  public sep:number;
+  public oct:number;
   public nov:number;
   public dec:number;
+  public debits1=new Array();
+  public debits2=new Array();
+  public debits3=new Array();
+  public debits4=new Array();
+  public debits5=new Array();
+  public debits6=new Array();
+  public debits7=new Array();
+  public debits8=new Array();
+  public debits9=new Array();
+  public debits10=new Array();
   public debits11=new Array();
+  public debits12=new Array();
+
   public debit=0;
   public Allmonths=new Array();
-  constructor(private ligneEcritureService:LigneEcritureServiceService,private datepipe:DatePipe) {}
+  constructor(private ligneEcritureService:LigneEcritureServiceService) {}
 
   ngOnInit() {
+  
+    this.DataGraph();
+    
+  }
 
-    this.findAll();
 
 
-
-
+Methodegraph(){
+  console.log("Second step");
     var gradientChartOptionsConfigurationWithTooltipBlue: any = {
       maintainAspectRatio: false,
       legend: {
@@ -414,18 +437,23 @@ export class DashboardComponent implements OnInit {
     //   [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
     // ];
     // this.data = this.datasets[1];
+    
     this.data=this.Allmonths
-    console.log(this.Allmonths)
-    console.log(this.data)
+    console.log(this.Allmonths.length)
+    console.log("tables: ->"+this.Allmonths)
+    // console.log(this.data)
 
     this.canvas = document.getElementById("chartBig1");
     this.ctx = this.canvas.getContext("2d");
 
     var gradientStroke = this.ctx.createLinearGradient(0, 230, 0, 50);
 
-    gradientStroke.addColorStop(1, 'rgba(233,32,16,0.2)');
-    gradientStroke.addColorStop(0.4, 'rgba(233,32,16,0.0)');
-    gradientStroke.addColorStop(0, 'rgba(233,32,16,0)'); //red colors
+    // gradientStroke.addColorStop(1, 'rgba(233,32,16,0.2)');
+    // gradientStroke.addColorStop(0.4, 'rgba(233,32,16,0.0)');
+    // gradientStroke.addColorStop(0, 'rgba(233,32,16,0)'); //red colors
+    gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+    gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+    gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
 
     var config = {
       type: 'line',
@@ -435,13 +463,14 @@ export class DashboardComponent implements OnInit {
           label: "My First dataset",
           fill: true,
           backgroundColor: gradientStroke,
-          borderColor: '#ec250d',
+          hoverBackgroundColor: gradientStroke,
+          borderColor: '#1f8ef1',
           borderWidth: 2,
           borderDash: [],
           borderDashOffset: 0.0,
-          pointBackgroundColor: '#ec250d',
+          pointBackgroundColor: '#1f8ef1',
           pointBorderColor: 'rgba(255,255,255,0)',
-          pointHoverBackgroundColor: '#ec250d',
+          pointHoverBackgroundColor: '#1f8ef1',
           pointBorderWidth: 20,
           pointHoverRadius: 4,
           pointHoverBorderWidth: 15,
@@ -486,89 +515,105 @@ export class DashboardComponent implements OnInit {
       options: gradientBarChartConfiguration
     });
 
+    
+  
   }
+
+
   public updateOptions() {
     this.myChartData.data.datasets[0].data = this.data;
     this.myChartData.update();
   }
 
 
-  findAll():void {
+  DataGraph():void {
     this.ligneEcritureService.findAll().subscribe((data: LigneEcriture[])=>{
       this.ligneEcritures=data;
-      
-      // console.log(this.ligneEcritures.map(debits=>debits.debit));
-      this.debits.push(this.ligneEcritures.map(debits=>debits.virement));
-      console.log("debits: " +this.debits);
-
-
-      // console.log(this.ligneEcritures.map(months=>months.dateEcriture));
-      // this.months.push(this.ligneEcritures.map(months=>months.dateEcriture       
-      //   ));
-      //   console.log("months: "+this.months);
      
-
         for(let i=0;i<this.ligneEcritures.length;i++){
         
         var debit=this.ligneEcritures[i].virement;
         var slices=this.ligneEcritures[i].dateEcriture;
-        // console.log("slice "+slices.toString().slice(5,7));
-
        
         var date=new Date(slices);
         var dtm=date.getMonth()+1;
-        console.log("Mois "+dtm);
-        
+              
+        if(dtm==1){
+          this.debits1.push(debit);
+          this.jan=this.debits1.reduce((a,b)=>a+b);
+        }  
 
-
-         if(dtm==11){
+        else if(dtm==2){
+          this.debits2.push(debit);
+          this.fev=this.debits2.reduce((a,b)=>a+b);
+        }  
+        else if(dtm==3){
+          this.debits3.push(debit);
+          this.mars=this.debits3.reduce((a,b)=>a+b);
+        }  
+       else if(dtm==4){
+          this.debits4.push(debit);
+          this.avr=this.debits4.reduce((a,b)=>a+b);
+        }  
+       else if(dtm==5){
+          this.debits5.push(debit);
+          this.mai=this.debits5.reduce((a,b)=>a+b);
+        }  
+        else if(dtm==6){
+          this.debits6.push(debit);
+          this.juin=this.debits6.reduce((a,b)=>a+b);
+        }  
+        else if(dtm==7){
+          this.debits7.push(debit);
+          this.juil=this.debits7.reduce((a,b)=>a+b);
+        }  
+        else if(dtm==8){
+          this.debits8.push(debit);
+          this.aout=this.debits8.reduce((a,b)=>a+b);
+        }  
+        else if(dtm==9){
+          this.debits9.push(debit);
+          this.sep=this.debits9.reduce((a,b)=>a+b);
+        }  
+        else if(dtm==10){
+          this.debits10.push(debit);
+          this.oct=this.debits10.reduce((a,b)=>a+b);
+        }  
+        else if(dtm==11){
           this.debits11.push(debit);
           this.nov=this.debits11.reduce((a,b)=>a+b);
-          console.log("nov "+this.nov);
-         
+                  
          }
          else if(dtm==12){
           this.debits12.push(debit);
           this.dec=this.debits12.reduce((a,b)=>a+b);
-          console.log("dec "+this.dec);
-          
+                   
          }
          
-         console.log(debit);
-        }
-     
-        console.log("Debits nov: "+this.nov)
-        console.log("Debit dec: "+this.dec)
-      
-        this.Allmonths.push(this.nov,this.dec);
-        console.log(this.Allmonths);
         
-        this.Allmonths=this.datasets;
-
-      // for(let i=0;i<this.months.length;i++){
-      //   console.log(this.months.length)
-      //   var slices=this.months[i];
-      //   console.log("slice "+slices[i].slice(5,7));
-
-      // }
-
-      var test=new Array();
-      // this.months.forEach(function(value){
-      //   value.getMonth();
-      //   console.log("here :"+value.getMonth());
-      // })
-    //  var testdate=new Date();
-    //  console.log("here dateMonth: "+(testdate.getMonth()+1));
-
-    //   console.log(this.ligneEcritures.forEach(()=>this.ligneEcriture.dateEcriture.getMonth()));
-    //   console.log(this.ligneEcritures.map(months=>(months.dateEcriture.getMonth()+1)).slice(-2));
-                 
-    //     ;
+        }
+          
+        this.Allmonths.push(this.jan);
+        this.Allmonths.push(this.fev);
+        this.Allmonths.push(this.mars);
+        this.Allmonths.push(this.avr);
+        this.Allmonths.push(this.mai);
+        this.Allmonths.push(this.juin);
+        this.Allmonths.push(this.juil);
+        this.Allmonths.push(this.aout);
+        this.Allmonths.push(this.sep);
+        this.Allmonths.push(this.oct);
+        this.Allmonths.push(this.nov);
+        this.Allmonths.push(this.dec);
+        
+        
+        
+        this.Methodegraph();
     })
+
+   
+
+    
   }
-
-
-
-
 
 }
